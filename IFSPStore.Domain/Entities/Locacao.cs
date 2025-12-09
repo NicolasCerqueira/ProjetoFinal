f83c66a72d66ea47511b0a5220a6366949b559cc
Nicolas Cerqueira <ncerqueira122@gmail.com>
2025-12-09 10:58:46 -0300

    -retirando erros

--- IFSPStore.Domain/Entities/Locacao.cs
+++ IFSPStore.Domain/Entities/Locacao.cs
@@ -1,5 +1,5 @@
 ﻿using IFSPStore.Domain.Base;
-using Microsoft.Extensions.Options;
+using System.Collections.Generic;
 
 namespace IFSPStore.Domain.Entities
 {
@@ -9,6 +9,7 @@ namespace IFSPStore.Domain.Entities
         {
             CarrosAlugados = new List<CarrosAlugados>();
         }
+
         public Locacao(int id, DateTime dataLocacao, DateTime dataDevolucao, decimal valorTotal,
             int numDiarias, Funcionario funcionario, Cliente cliente)
             : base(id)
@@ -26,34 +27,23 @@ namespace IFSPStore.Domain.Entities
         public DateTime DataDevolucao { get; set; }
         public decimal ValorTotal { get; set; }
         public int NumDiarias { get; set; }
-        public int FuncionarioId { get; set; } // FK para User
-        public int ClienteId { get; set; } // FK para Customer
-        public Funcionario Funcionario { get; set; }
-        public Cliente Cliente { get; set; }
+        public int FuncionarioId { get; set; }
+        public Funcionario? Funcionario { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente? Cliente { get; set; }
+
         public List<CarrosAlugados> CarrosAlugados { get; set; }
     }
-
     public class CarrosAlugados : BaseEntity<int>
     {
-            public CarrosAlugados()
-            {
-                
-            }
-            public CarrosAlugados(int id, Locacao? locacao, Carro? carro,
-                decimal valorDiaria, decimal valorTotalLoc) : base(id)
-            {
-                Locacao_ = locacao;
-                Carro = carro;
-                ValorDiaria = valorDiaria;
-                ValorTotalLoc = valorTotalLoc;
-            }
+        public CarrosAlugados() { }
 
-            public int LocacaoId { get; set; }
-            public Locacao? Locacao_ { get; set; }
-            public int CarroId { get; set; }
-            public virtual Carro? Carro { get; set; }
-            public decimal ValorDiaria { get; set; }
-            public decimal ValorTotalLoc { get; set; }  // (ValorDiaria * Dias)
-        
+        public int LocacaoId { get; set; }
+        public virtual Locacao? Locacao_ { get; set; }
+        public int CarroId { get; set; }
+        public virtual Carro? Carro { get; set; }
+        public decimal ValorDiaria { get; set; }
+        public decimal ValorTotalLoc { get; set; }
+        public decimal Quantidade { get; set; }
     }
 }
